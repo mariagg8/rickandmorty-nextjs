4f9d84d514c176039db24e9790d5ea590e4d5811
Maria Gonzalez <chelo140581@gmail.com>
2022-05-11 16:18:07 +0200

    fix google font link

--- pages/_document.js
+++ pages/_document.js
@@ -8,18 +8,13 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin="true"
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:wght@100&display=swap"
           rel="stylesheet"
         ></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin
-        ></link>
+
         <link
           href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:wght@100&display=swap"
           rel="stylesheet"
